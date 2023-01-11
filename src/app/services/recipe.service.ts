8a67a64dc7a0d165bf4fcaefa2a55981a2de21bc
Naime Medjek <naimemedjek@gmail.com>
2023-01-11 15:40:19 +0100

    added getallrecipes method in recipe service

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Recipe } from '../models/recipe';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  apiUrl = 'https://localhost:7299/api/';
+
+  getAllRecipes(): Observable<Recipe[]> {
+    return this.http.get<Recipe[]>(this.apiUrl);
+  }
 }
