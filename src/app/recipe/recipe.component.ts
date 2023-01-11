e8f48d0b3a4aaf2d73d468ead6adffbee069b9d6
Naime Medjek <naimemedjek@gmail.com>
2023-01-11 16:40:01 +0100

    added getrecipe in recipe component

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RecipeService } from '../services/recipe.service';
 
 @Component({
   selector: 'app-recipe',
@@ -7,7 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeComponent implements OnInit {
 
+constructor(private recipeService: RecipeService, private activeRoute: ActivatedRoute) {}
 
+id = this.activeRoute.snapshot.params['id'];
+recipe: any = {};
+
+getRecipe() {
+  this.recipeService.onGetRecipe(this.id).subscribe(
+    response => {
+      console.log(response);
+    }
+  )
+}
   
   
   
