b47773dd4a3bee52f4082a539744641d93b00649
Naime Medjek <naimemedjek@gmail.com>
2023-01-13 19:10:10 +0100

    edited enums

--- src/app/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Category, CourseType, Recipe } from '../models/recipe';
 import { RecipeService } from '../services/recipe.service';
 
 @Component({
@@ -8,29 +9,32 @@ import { RecipeService } from '../services/recipe.service';
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
+  
+  constructor(private recipeService: RecipeService, private actRoute: ActivatedRoute, private router: Router){}
+  
+  recipe: any = {};
+  courseTypes: any = CourseType;
+  categories: any = Category;
+  keys = Object.keys;
+  id = this.actRoute.snapshot.params['id'];
 
-constructor(private recipeService: RecipeService, private actRoute: ActivatedRoute, private router: Router){}
-
-recipe: any = {};
-id = this.actRoute.snapshot.params['id'];
 
 editRecipe() {
-this.recipeService.onPutRecipe(this.recipe).subscribe(
-  response => {
-    console.log(response);
-  }
-)
-}
-
-getRecipe() {
-  this.recipeService.onGetRecipe(this.id).subscribe(
+  this.recipeService.onPutRecipe(this.recipe).subscribe(
     response => {
-      this.recipe = response;
+      this.router.navigate(['/recipes']);
     }
-  )
-}
-
-ngOnInit(): void {
-  this.getRecipe()
-}
-}
+    )
+  }
+  
+  getRecipe() {
+    this.recipeService.onGetRecipe(this.id).subscribe(
+      response => {
+        this.recipe = response;    }
+        )
+      }
+      
+      ngOnInit(): void {
+        this.getRecipe()
+      }
+    }
