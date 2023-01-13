459ca8c32cdb94a251b1cc3f03cc23d89768f033
Naime Medjek <naimemedjek@gmail.com>
2023-01-13 08:10:03 +0100

    forms added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import {HttpClientModule} from '@angular/common/http';
 import { RecipeComponent } from './recipe/recipe.component';
 import { RecipeCreateComponent } from './recipe-create/recipe-create.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
